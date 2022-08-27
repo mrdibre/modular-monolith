b6ab3a79cdfea68311df8ebd626c74d33b2dc2f3
César Felipe <cesar.felp982@gmail.com>
2022-08-27 10:56:50 -0300

    removing unused imports

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -1,8 +1,5 @@
 import { Sequelize } from "sequelize-typescript";
 import { ProductModel } from "../repository/product.model";
-import { ProductRepository } from "../repository/product.repository";
-import { AddProductUseCase } from "../usecase/add-product/add-product";
-import { ProductAdmFacade } from "./product-adm-facade";
 import { ProductAdmFacadeFactory } from "../factory/facade.factory";
 
 describe("Product Adm Facade Test", function () {
